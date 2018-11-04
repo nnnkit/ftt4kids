7c8e8f91793b2a1032f39186aeb294b4136b47ef
Vaibhav Mule <vaibhavmule135@gmail.com>
2018-11-04 16:40:00 +0530

    Remove frontend folder, add login required to index

--- ftt4kids/urls.py
+++ ftt4kids/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name="index.html")),
+    path('', login_required(TemplateView.as_view(template_name="index.html"))),
 ]
